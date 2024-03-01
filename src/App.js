a8526108492f364ee732dc85b8e995a627dd7bad
Camila Lopes <camilalopesqta@gmail.com>
2024-02-29 23:26:56 -0300

    Exercícios da aula de useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import { Garagem } from './Componentes/Garagem'
-import { GlobalStyled } from './GlobalStyled'
+import { Garagem } from "./Componentes/Garagem";
+import { GlobalStyled } from "./GlobalStyled";
+import { useState } from "react";
 
 export default function App() {
+  const [nome, setNome] = useState("Camila");
+  const [automovel, setAutomovel] = useState({
+    modelo: "Fusca",
+    cor: "Azul",
+    ano: 1970,
+    flex: false,
+    adicionadoPor: "Camila",
+  },
+  );
+
+  const carro1 = {
+    modelo: "Etios",
+    cor: "Prata",
+    ano: 2017,
+    flex: true,
+    adicionadoPor: "Jacob"
+  }
+
+  const carro2 = {
+    modelo: "Yaris",
+    cor: "Preto",
+    ano: 2022,
+    flex: true,
+    adicionadoPor: "Camila"
+  }
+
+  const carro3 = {
+    modelo: "Civic",
+    cor: "Preto",
+    ano: 2018,
+    flex: true,
+    adicionadoPor: "Camila"
+  }
+
+  
+  
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome="Labenu" />
+      <Garagem 
+      nome={nome} 
+      automovel={automovel}
+      setNome={setNome}
+      setAutomovel={setAutomovel}
+      carro1={carro1}
+      carro2={carro2}
+      carro3={carro3}
+      />
     </div>
-  )
+  );
 }
--- src/Componentes/Garagem/index.js
+++ src/Componentes/Garagem/index.js
@@ -1,20 +1,29 @@
 import { Carro } from '../Carro'
 import { Botao, Estacionamento, GaragemContainer } from './styles'
 
-export function Garagem({ nome }) {
+export function Garagem({ nome, automovel, setNome, setAutomovel, carro1, carro2, carro3 }) {
+  const alterarNome = () => {
+    setNome("Julieta")
+  }
+
+  const alterarCarro = () => {
+    setAutomovel(carro2)
+  }
+
   return (
     <GaragemContainer>
       <h1>Garagem da {nome}</h1>
-      <Botao>Muda nome</Botao>
-
+      <Botao onClick={alterarNome}>Muda nome</Botao>
+      <Botao onClick={alterarCarro}>Alterar Carro</Botao>
       <Estacionamento>
         <Carro
-          modelo="Corsa"
-          cor="branco"
-          ano={2020}
-          adicionadoPor="Labenu"
-          flex
+          modelo={automovel.modelo}
+          cor={automovel.cor}
+          ano={automovel.ano}
+          adicionadoPor={automovel.adicionadoPor}
+          flex= {automovel.flex}
         />
+        
       </Estacionamento>
     </GaragemContainer>
   )
